d5e126a9593a8a0c2c31da1945cd15cc9ec7d424
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2017-10-19 00:39:26 +0200

    Button interrupt

--- Src/stm32f4xx_it.c
+++ Src/stm32f4xx_it.c
@@ -72,13 +72,9 @@ void SysTick_Handler(void)
 */
 void EXTI0_IRQHandler(void)
 {
-  /* USER CODE BEGIN EXTI0_IRQn 0 */
-
-  /* USER CODE END EXTI0_IRQn 0 */
   HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
-  /* USER CODE BEGIN EXTI0_IRQn 1 */
+	HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
 
-  /* USER CODE END EXTI0_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
